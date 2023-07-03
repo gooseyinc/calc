a7787802cb11c36d79ed6ae6512a2cdcb5af5167
Ozgur <o@zgur.org>
2023-07-03 20:52:08 +0400

    Create aritmetik.go

---
+++ aritmetik.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+func main() {
+
+  number1 := 6
+  number2 := 2
+  
+  toplama := number1 + number2
+  fmt.Printf("%d + %d = %d\n", number1, number2, summary)
+
+  cikartma := number1 - number2
+  fmt.Printf("%d - %d = %d\n",number1, number2,  difference)
+
+  carpma := number1 * number2
+  fmt.Printf("%d * %d is %d\n",number1, number2,  carpma)
+
+}
